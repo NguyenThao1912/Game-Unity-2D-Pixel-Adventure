4e9237275683d2831c239adb06b0f3fa6048e3dc
Nguyễn Hữu Thảo <49112998+NguyenThao1912@users.noreply.github.com>
2021-04-13 16:21:19 +0700

    init the game
    give m best wish for my bros

---
+++ Pixel Adventure/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRespawn : MonoBehaviour
+{
+
+    private float checkPointPositionX, checkPointPositionY;
+
+    public Animator animator;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetFloat("checkPointPositionX") != 0)
+        {
+            transform.position = (new Vector2(PlayerPrefs.GetFloat("checkPointPositionX"), PlayerPrefs.GetFloat("checkPointPositionY")));
+        }
+    }
+
+    //Lưu vị trí người chơi để hồi sinh
+    public void ReachedCheckPoint(float x,float y)
+    {
+        PlayerPrefs.SetFloat("checkPointPositionX",x);
+        PlayerPrefs.SetFloat("checkPointPositionY", y);
+    }
+
+    //Khi người chơi chết sẽ quay lại từ đầu màn chơi
+    public void PlayerDeath()
+    {
+        animator.Play("Hit");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
